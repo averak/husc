f3d37099c31dfb9c31f4bded8913dc84cd3bec50
AjxLab <abe12@mccc.jp>
2020-01-11 02:43:59 +0900

    currrent urlを自動更新

--- lib/husc.rb
+++ lib/husc.rb
@@ -183,6 +183,7 @@ class Husc
 
   def update_params(html)
     ## -----*----- パラメータを更新 -----*----- ##
+    @url = @agent.page.uri
     @html = html
     @doc = Nokogiri::HTML.parse(@html)
     table_to_hash
